bc2af8db2f63dfac8e76fae65e671ee46cdce387
vineet <vineetver@hotmail.com>
2022-08-04 17:37:49 -0400

    Setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='text2emotion',
+    version='0.1',
+    description='This package contains the code for the Text 2 emotions',
+    author='Vineet Verma',
+    author_email='vineetver@hotmail.com',
+    packages=find_packages(exclude=['tests']),
+    install_requires=[
+        'emoji',
+        'tensorflow',
+        'transformers',
+        'pandas',
+        'scikit-learn',
+        'matplotlib',
+        'plotly',
+        'seaborn',
+        'pyspark'],
+    entry_points={
+        'console_scripts': [
+            'clean_data = main.clean:main',
+            'feature_engineering = main.feature_engineering:main',
+            'train_model = main.train_model:main',
+            'make_predictions = main.make_predictions:main',
+        ],
+    }
+)
